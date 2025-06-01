49a027988d6b1a4217268ca739b2bfe00cb16271
dishaparam03 <dishaparmeshwa03@gmail.com>
2025-06-01 09:41:01 +0530

    test commit
    test commit

---
+++ prgm2-example-jar/src/main/java/com/prgm2/maven/prgm2_example_jar/App.java
@@ -0,0 +1,13 @@
+package com.prgm2.maven.prgm2_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi Disha, Welcome !" );
+    }
+}
